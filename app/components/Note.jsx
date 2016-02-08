08fb1f4d557a86617cf209af1f04ce7c7626fe03
Gyula Szalai <gyszalai@gmail.com>
2016-02-08 18:37:01 +0100

    Added propTypes to Note react component

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default class Note extends React.Component {
     render() {
         return (
-            <div>Megjegyzés: {this.props.text}</div>
+            <div>{'Megjegyzés: '}{this.props.text}{' '}{this.props.myEnum}</div>
         );
     }
 };
+
+Note.propTypes = {
+    text: React.PropTypes.string.isRequired,
+    myEnum: React.PropTypes.oneOf(['A', 'B'])
+}
